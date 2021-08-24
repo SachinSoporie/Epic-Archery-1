570bc56885d7c74874856c018b142e4ecb082fd3
SachinSoporie <85801708+SachinSoporie@users.noreply.github.com>
2021-08-24 12:53:06 -0400

    Update ComputerPlayer.js

--- ComputerPlayer.js
+++ ComputerPlayer.js
@@ -7,7 +7,7 @@ class ComputerPlayer {
           this.body = Bodies.rectangle(x, y, width, height , options);
           this.width = width;
           this.height = height;
-          this.image = loadImage("./assets/base2.png");
+          this.image = loadImage("./assets/player.png");
 
           World.add(world, this.body);
     }
